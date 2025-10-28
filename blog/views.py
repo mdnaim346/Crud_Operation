11b1c843b7e4032e617fa342db54280ad9207598
Naim Reza <mdnaimreza66@gmail.com>
2025-10-28 22:00:45 +0600

    Blog Post Created

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,32 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import MyPost
 
-def home(request):
-    return HttpResponse("Hello, Naim! Welcome to your first Django app 🎉")
+# READ
+def post_list(request):
+    posts = MyPost.objects.all()
+    return render(request, 'post_list.html', {'posts': posts})
+
+# CREATE
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        MyPost.objects.create(title=title, content=content)
+        return redirect('post_list')
+    return render(request, 'post_form.html')
+
+# UPDATE
+def post_update(request, id):
+    post = get_object_or_404(MyPost, id=id)
+    if request.method == 'POST':
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('post_list')
+    return render(request, 'post_form.html', {'post': post})
+
+# DELETE
+def post_delete(request, id):
+    post = get_object_or_404(MyPost, id=id)
+    post.delete()
+    return redirect('post_list')
